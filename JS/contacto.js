7df30d8b81d05661d0199a54769b0d1e8dc8fb08
MaximoColina <2mxmcln@gmail.com>
2023-05-20 15:19:37 -0300

    nuevo footer

--- JS/contacto.js
+++ JS/contacto.js
@@ -9,6 +9,7 @@ const comentariosInput = document.querySelector('#comentarios');
 form.addEventListener('submit', (event) => {
 event.preventDefault();
 
+
   // Validamos los campos del formulario
 if (!validarApellido()) {
     alert('Ingrese un apellido valido');
@@ -16,11 +17,13 @@ if (!validarApellido()) {
     return;
 }
 
+
 if (!validarNombre()) {
     alert('Ingrese un nombre valido');
     nombreInput.focus();
     return;
 }
+
 
 if (!validarCorreo()) {
     alert('Ingrese un correo electrónico valido');
@@ -28,11 +31,13 @@ if (!validarCorreo()) {
     return;
 }
 
+
 if (!validarComentarios()) {
     alert('Ingrese un comentario valido');
     comentariosInput.focus();
     return;
 }
+
 
 // Si todo está validado, enviamos el formulario
 form.submit();
@@ -45,22 +50,26 @@ const apellido = apellidoInput.value.trim();
 const regex = /^[a-zA-Z\s]+$/;
 return regex.test(apellido) && apellido.length > 0 && apellido.length <= 40;
 }
-  
+
+
 function validarNombre() {
 const nombre = nombreInput.value.trim();
 const regex = /^[a-zA-Z\s]+$/;
 return regex.test(nombre) && nombre.length > 0 && nombre.length <= 40;
 }
-  
+
+
 function validarCorreo() {
 const correo = correoInput.value.trim();
 const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 return regex.test(correo) && correo.length > 0 && correo.length <= 40;
 }
-  
+
+
 function validarComentarios() {
 const comentarios = comentariosInput.value.trim();
 return comentarios.length > 0 && comentarios.length <= 250;
 }
-  
+
+
 
